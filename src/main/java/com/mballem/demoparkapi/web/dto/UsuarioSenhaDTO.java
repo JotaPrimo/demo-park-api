4ff3c8bb332fdc02e11a48f9c21be41e70c2ca0e
JotaPrimo <gestald118@gmail.com>
2024-01-21 17:39:57 -0400

    27. Validando o objeto UsuarioSenhaDto

--- src/main/java/com/mballem/demoparkapi/web/dto/UsuarioSenhaDTO.java
+++ src/main/java/com/mballem/demoparkapi/web/dto/UsuarioSenhaDTO.java
@@ -1,5 +1,7 @@
 package com.mballem.demoparkapi.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,7 +11,15 @@ import lombok.*;
 @AllArgsConstructor
 public class UsuarioSenhaDTO {
 
+    @NotBlank(message = "Senha é obrigatório")
+    @Size(min = 6, max = 6, message = "Deve ter 6 caracteres")
     private String senhaAtual;
+
+    @NotBlank(message = "Nova senha é obrigatório")
+    @Size(min = 6, max = 6, message = "Deve ter 6 caracteres")
     private String novaSenha;
+
+    @NotBlank(message = "Confirma senha é obrigatório")
+    @Size(min = 6, max = 6, message = "Deve ter 6 caracteres")
     private String confirmaSenha;
 }
