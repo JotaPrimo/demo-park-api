9392b9c75f5e8181326ecb185e35bee11716da9b
Jota Santos <jailsonsantosdpge@outlook.com>
2024-01-22 14:24:15 -0400

    42. Teste para verificar exceções de validações (422)

--- src/test/java/com/mballem/demoparkapi/UsuarioIT.java
+++ src/test/java/com/mballem/demoparkapi/UsuarioIT.java
@@ -3,10 +3,13 @@ package com.mballem.demoparkapi;
 import com.mballem.demoparkapi.web.controller.UsuarioController;
 import com.mballem.demoparkapi.web.dto.UsuarioCreateDTO;
 import com.mballem.demoparkapi.web.dto.UsuarioResponseDTO;
+import com.mballem.demoparkapi.web.exception.ErrorMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -35,6 +38,78 @@ public class UsuarioIT {
         Assertions.assertThat(responseBody.getId()).isNotNull(); // checando se a resposta de retorno tem um id não é null
         Assertions.assertThat(responseBody.getUsername()).isEqualTo("tody@gmail.com"); // checando se o username de retorno é igual ao paramentro
         Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENTE"); // checando se o username de retorno é igual ao paramentro
+    }
+
+    @Test
+    public void createUsuarioComUsernameInvalidoRetornarErrorMessageStatus422() {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri(UsuarioController.URI_USUARIOS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("", "12345678"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri(UsuarioController.URI_USUARIOS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("tody@", "12345678"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    @Test
+    public void createUsuarioComPasswordInvalidoRetornarErrorMessageStatus422() {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri(UsuarioController.URI_USUARIOS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("tedsantos@gmail.com", ""))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = testClient
+                .post()
+                .uri(UsuarioController.URI_USUARIOS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("tody@", "123"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+
+        responseBody = testClient
+                .post()
+                .uri(UsuarioController.URI_USUARIOS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDTO("tody@", "1233456789ldjfhnsweurpnbsdkljkmçsjpuwrjewrnerhjfsdf"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
 
 }
